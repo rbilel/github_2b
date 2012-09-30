d66dfa08c57a82d3f365b8ce564d08e57279c649
rbilel <rbilel@live.fr>
2012-09-30 02:02:29 +0100

    first commit

---
+++ Hello/src/com/isamm/j2ee/Hello.java
@@ -0,0 +1,13 @@
+package com.isamm.j2ee;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+	}
+
+}
