212580d6bef0351f445f7c9fe48326326a44c2c4
rbilel <rbilel@live.fr>
2012-10-29 21:51:36 +0100

    By Me

---
+++ Vente/src/com/isamm/model/admin_.java
@@ -0,0 +1,12 @@
+package com.isamm.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2012-10-27T00:28:06.646+0100")
+@StaticMetamodel(admin.class)
+public class admin_ {
+	public static volatile SingularAttribute<admin, Long> id;
+	public static volatile SingularAttribute<admin, String> nom;
+}
